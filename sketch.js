afa6512a608502980a0a5a0bd9f99839838c4e9f
GarfieldStan94 <71049441+GarfieldStan94@users.noreply.github.com>
2020-11-18 19:36:35 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+var dustbin
+var paper
+var ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1600, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	ground= new Ground(800,680,1600,20);
+	paper= new Paper(200,450,70);
+	dustbin= new Dustbin(1200,650)
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background("white");
+  paper.display();
+  dustbin.display();
+  ground.display();
+
+  
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+
+	  Matter.Body.applyForce(paper.body,paper.body.position,{x:85,y:-85});
+  
+	}
+}
+
+
+
